2c448f9d56421c223c2e064b1a80d718f5a6f812
traitorwtf <kkurt@mail.ru>
2017-04-03 19:03:56 +0400

    new commit

---
+++ src/main/java/com/tratorwtf/db/User.java
@@ -0,0 +1,64 @@
+package com.tratorwtf.db;
+
+/**
+ * Created by traitorwtf on 03.04.2017.
+ */
+public class User {
+    private int id;
+    private String name;
+    private String surname;
+    private int age;
+    private String city;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", age=" + age +
+                ", city='" + city + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/tratorwtf/db/mainDB.java
+++ src/main/java/com/tratorwtf/db/mainDB.java
@@ -3,6 +3,7 @@ package com.tratorwtf.db;
 //import com.mysql.fabric.jdbc.FabricMySQLDriver;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 /**
  * Created by traitorwtf on 03.04.2017.
@@ -14,6 +15,8 @@ public class mainDB {
 
     public static void main(String[] args) {
         Connection connection;
+        String DELETE = "delete from users where id = ?";
+
         try {
             //Driver driver = new FabricMySQLDriver();
             //DriverManager.registerDriver(driver);
@@ -27,10 +30,53 @@ public class mainDB {
 //                System.out.println("connection is closed");
 //            }
 
-            //statement.execute("insert into users(name, surname, age, city) values('Alex', 'Maksimova', 16, 'Glazov')");
-            //statement.executeUpdate("update users set name = 'Alexandra' where name = 'Alex'");
+//            statement.execute("insert into users(name, surname, age, city) values('Alex', 'Maksimova', 16, 'Glazov')");
+//            statement.executeUpdate("update users set name = 'Alexandra' where name = 'Alex'");
+            ResultSet resultSet = statement.executeQuery("Select * from users");
+
+//            statement.addBatch("insert into users(name, surname, age, city) values('Name1', 'Surname1', 66, 'City1')");
+//            statement.addBatch("insert into users(name, surname, age, city) values('Name2', 'Surname2', 66, 'City2')");
+//            statement.executeBatch();
+//            statement.clearBatch();
+
+//            statement.getConnection();
+//            System.out.println(statement.isClosed());
+//            statement.close();
+//            System.out.println(statement.isClosed());
+
+            ArrayList<User> users = new ArrayList<User>();
+
+            while (resultSet.next()){
+                User user = new User();
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                String surname = resultSet.getString("surname");
+                int age = resultSet.getInt("age");
+                String city = resultSet.getString("city");
+
+                user.setId(id);
+                user.setName(name);
+                user.setSurname(surname);
+                user.setAge(age);
+                user.setCity(city);
+
+                users.add(user);
+            }
+
+            for (User us: users) {
+                System.out.println(us.toString());
+            }
+
+            // Использование prepareStatemnet'ов.
+//            PreparedStatement preparedStatement = connection.prepareStatement(DELETE);
+//            preparedStatement.setInt(1, 10);
+//            preparedStatement.executeUpdate();
+
+            connection.close();
+
         } catch (SQLException e) {
             System.err.println("Не удалось загрузить класс драйвера");
         }
+
     }
 }
